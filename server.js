ce4d72be927f32ea7c098349994342c8004c5a49
Barento Muhedin <awesomemoi@hotmail.com>
2025-02-19 22:22:13 -0500

    Initialized tailwind cli

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express(); //Initialize Express
-const mongoose = require("mongoose");//doing nothing for now
+const mongoose = require("mongoose"); //doing nothing for now
 const logger = require("morgan");
 // const connectDB = require(".config/database");
 const mainRoutes = require("./routes/main");
+const loginRoutes = require("./routes/login");
 
 require("dotenv").config({ path: "./config/.env" });
 
@@ -14,11 +15,11 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 //Body Parsing
-app.use(express.urlencoded({extended: true}))
-app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 //Logger
-app.use(logger('dev'))
+app.use(logger("dev"));
 
 //Skip Passport for now
 
@@ -29,11 +30,10 @@ app.use(logger('dev'))
 //Skip Flash
 
 // Routes
-app.use('/', mainRoutes)
-
+app.use("/", mainRoutes);
+app.use("/login", loginRoutes);
 
 //Server listening
 app.listen(process.env.PORT, () => {
-  console.log("Server is running on port " + process.env.PORT)
-})
-
+	console.log("Server is running on port " + process.env.PORT);
+});
